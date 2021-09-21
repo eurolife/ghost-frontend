5ff0e924655404f571bae0eef4bf9566c16ff06b
Paula Hightower <eurolife@gmail.com>
2021-09-21 12:03:09 +0100

    add dependencies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,40 @@
-module.exports = {
-  reactStrictMode: true,
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+  [
+    optimizedImages, {
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        speed: 3,
+        strip: true,
+        verbose: true,
+      },
+      imagesPublicPath: '/ghost-frontend/_next/static/images/',
+    }
+  ],
+  {
+  assetPrefix: '/ghost-frontend/',
+  basePath: '/ghost-frontend',
+  pageExtensions: ["tsx"],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          type: "json",
+          use: "yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+      ]
+    );
+    return config;
+  },
 }
+]);
+
